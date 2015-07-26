18169dd2950ecf3c1285fd65fd3cab26666137aa
kaiserxzero <devstephenbecht@gmail.com>
2015-07-26 13:22:19 -0400

    adding diceroller.py

---
+++ diceroller.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+import time
+import Skype4Py
+import random
+
+
+def commands(Message, Status):
+	if Status == 'SENT' or (Status == 'RECEIVED'):
+
+		if Message.Body == "//*":
+			diceroll(Message)
+
+		elif Message.Body == "!ping":
+			cmd_ping(Message)
+
+		elif Message.Body == "!rickroll":
+			cmd_rickroll(Message)
+
+		elif Message.Body == "!celebrate":
+			cmd_celebrate(Message)
+
+		elif Message.Body == "!hammertime":
+			cmd_hammertime(Message)
+
+		elif Message.Body == "!credit":
+			cmd_credit(Message)
+
+		elif Message.Body == "!help":
+			cmd_help(Message)
+
+		elif Message.Body == "!spam":
+			cmd_spam(Message)
+
+		elif Message.Body == "!introduce":
+			cmd_intro(Message)
+
+		elif Message.Body == '!dice':
+			cmd_dice(Message)
+
+		else:
+			pass
+
+	else:
+		pass
+
+
+def diceroll(Message):
+	test=1
+
+
+
+def cmd_ping(Message):
+	Message.Chat.SendMessage('Robot: Yes, I\'m still alive. :)')
+	print "Ping Command Received \n"
+
+def cmd_rickroll(Message):
+	Message.Chat.SendMessage('Robot: Never Gonna Give You Up! (dance)')
+	time.sleep(0.5)
+	Message.Chat.SendMessage('Robot: Never Gonna Let You Down! (dance)')
+	time.sleep(0.5)
+	Message.Chat.SendMessage('Robot: Never Gonna Run Around and Desert You! (dance)')
+	time.sleep(0.5)
+	Message.Chat.SendMessage('Robot: Never Gonna Make You Cry! (dance)')
+	time.sleep(0.5)
+	Message.Chat.SendMessage('Robot: Never Gonna Say Goodbye! (dance)')
+	time.sleep(0.5)
+	Message.Chat.SendMessage('Robot: Never Gonna Tell A Lie! (dance)')
+	time.sleep(0.5)
+	Message.Chat.SendMessage('Robot: Or Hurt You! (dance)')
+	time.sleep(0.5)
+	print "Rick Command Received.\n"
+
+def cmd_celebrate(Message):
+	Message.Chat.SendMessage('Robot: Good job!')
+	time.sleep(1)
+	Message.Chat.SendMessage('Robot: You did great!')
+	time.sleep(1)
+	Message.Chat.SendMessage('Robot: Keep up the good work!')
+	time.sleep(1)
+	Message.Chat.SendMessage('Robot: (clap)')
+	print "Celebrate Command Received.\n"
+
+def cmd_hammertime(Message):
+	Message.Chat.SendMessage('Robot: EVERYONE!')
+	time.sleep(1)
+	Message.Chat.SendMessage('Robot: EVERYONE STOP WHAT YOU\'RE DOING!')
+	time.sleep(1)
+	Message.Chat.SendMessage('Robot: DO YOU GUYS EVEN KNOW WHAT TIME IT IS!?!?!?!?!?')
+	time.sleep(1)
+	Message.Chat.SendMessage('Robot: HAMMERTIME!!!!!!!! (dance)')
+	print "Hammer time Command Received.\n"
+
+def cmd_credit(Message):
+	Message.Chat.SendMessage('Robot: Hi, I\'m Snayer\'s Bot.')
+	Message.Chat.SendMessage('Robot: Everything on me was made by him.')
+	Message.Chat.SendMessage('Robot: If you want to further develop me, just ask Snayer!')
+	print "Credit Command Received.\n"
+
+def cmd_help(Message):
+	Message.Chat.SendMessage('Robot: Type !ping to see if the bot is alive!')
+	Message.Chat.SendMessage('Robot: Type !celebrate to have a party!')
+	Message.Chat.SendMessage('Robot: Type !rickroll to rick roll someone!')
+	Message.Chat.SendMessage('Robot: Type !hammertime to stop, drop, and hammertime!')
+	Message.Chat.SendMessage('Robot: Type !credit to see who made me!')
+	Message.Chat.SendMessage('Robot: Type !spam for some yummy spam!')
+	Message.Chat.SendMessage('Robot: Type !dice for a fun game!')
+	print 'Help Command Recieved.\n'
+
+def cmd_spam(Message):
+	print 'Spam Command Recieved.\n'
+	while True:
+		Message.Chat.SendMessage('Robot: SPAM IS YUMMY!')
+
+def cmd_intro(Message):
+	Message.Chat.SendMessage('Robot: Hi!')
+	time.sleep(2)
+	Message.Chat.SendMessage('Robot: My name is Willis.')
+	time.sleep(2)
+	Message.Chat.SendMessage('Robot: Techincally, I\'m a robot!')
+	time.sleep(2)
+	Message.Chat.SendMessage('Robot: Type !help to see what I can do. :)')
+	print "Introduce Command Received.\n"
+
+def cmd_dice(Message):
+	Message.Chat.SendMessage('Robot: Put a bet on numbers 1 through 6.')
+	time.sleep(8)
+	answer = random.randint(1,6)
+	Message.Chat.SendMessage('Robot: *rolls dice*')
+	time.sleep(1)
+	Message.Chat.SendMessage('Robot: The dice rolled the number:')
+	Message.Chat.SendMessage(answer)
+	print "Someone's playing dice. \n"
+
+skype = Skype4Py.Skype();
+skype.OnMessageStatus = commands
+
+if skype.Client.IsRunning == False:
+    skype.Client.Start()
+skype.Attach();
+
+print 'Skype Bot currently running on user',skype.CurrentUserHandle, "\n"
+
+while True:
+    raw_input('')
